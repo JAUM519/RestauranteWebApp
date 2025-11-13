82ccd728aa36af8ee2508c3ed5b2019b4fbd50cf
Juansec01 <juan_s.castillo_a@uao.edu.co>
2025-11-12 23:35:45 -0500

    feat: integrar historial de pedidos al enrutador del cliente

--- smart-restaurant/src/router/AppRouter.jsx
+++ smart-restaurant/src/router/AppRouter.jsx
@@ -1,49 +1,57 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from '../pages/Login.jsx'
-import Client from '../pages/Client.jsx'
-import Waiter from '../pages/Waiter.jsx'
-import Cook from '../pages/Cook.jsx'
-import Account from '../pages/Account.jsx'
-import NotFound from '../pages/NotFound.jsx'
-import ProtectedRoute from './ProtectedRoute.jsx'
-import RoleRoute from './RoleRoute.jsx'
-import SignupGoogle from '../pages/SignupGoogle.jsx'
-import AppShell from '../layout/AppShell.jsx'
-import RoleRedirector from './RoleRedirector.jsx'
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login.jsx";
+import SignupGoogle from "../pages/SignupGoogle.jsx";
+import Client from "../pages/Client.jsx";
+import Waiter from "../pages/Waiter.jsx";
+import Cook from "../pages/Cook.jsx";
+import Account from "../pages/Account.jsx";
+import NotFound from "../pages/NotFound.jsx";
+import AppShell from "../layout/AppShell.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
+import RoleRoute from "./RoleRoute.jsx";
+import RoleRedirector from "./RoleRedirector.jsx";
+import HistorialPedidos from "../views/cliente/HistorialPedidos.jsx";
 
 function Shell({ children }) {
-    return <AppShell>{children}</AppShell>
+  return <AppShell>{children}</AppShell>;
 }
 
 export default function AppRouter() {
-    return (
-        <Routes>
-            <Route path="/" element={<RoleRedirector />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/sg" element={<SignupGoogle />} />
+  return (
+    <Routes>
+      {/* Redirección inicial según rol */}
+      <Route path="/" element={<RoleRedirector />} />
 
-            {/* Usuarios autenticados con layout */}
-            <Route element={<ProtectedRoute />}>
-                {/* Cuenta, válido para cualquier rol */}
-                <Route path="/account" element={<Shell><Account /></Shell>} />
+      {/* Rutas públicas */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/sg" element={<SignupGoogle />} />
 
-                {/* Cliente (vista expandida sin AppShell) */}
-                <Route element={<RoleRoute allow="client" />}>
-                    <Route path="/client" element={<Client />} />
-                </Route>
+      {/* Rutas protegidas (solo usuarios autenticados) */}
+      <Route element={<ProtectedRoute />}>
+        {/* Página de cuenta (accesible por todos los roles) */}
+        <Route path="/account" element={<Shell><Account /></Shell>} />
 
-                {/* Mesero (vista expandida sin AppShell) */}
-                <Route element={<RoleRoute allow="waiter" />}>
-                    <Route path="/waiter" element={<Waiter />} />
-                </Route>
+        {/* Historial del cliente */}
+        <Route path="/historial" element={<Shell><HistorialPedidos /></Shell>} />
 
-                {/* Cocinero (vista expandida sin AppShell) */}
-                <Route element={<RoleRoute allow="cook" />}>
-                    <Route path="/cook" element={<Cook />} />
-                </Route>
-            </Route>
+        {/* Cliente */}
+        <Route element={<RoleRoute allow="client" />}>
+          <Route path="/client" element={<Client />} />
+        </Route>
 
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    )
+        {/* Mesero */}
+        <Route element={<RoleRoute allow="waiter" />}>
+          <Route path="/waiter" element={<Waiter />} />
+        </Route>
+
+        {/* Cocinero */}
+        <Route element={<RoleRoute allow="cook" />}>
+          <Route path="/cook" element={<Cook />} />
+        </Route>
+      </Route>
+
+      {/* Página no encontrada */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
