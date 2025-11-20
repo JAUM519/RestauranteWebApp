e7d39a14cdd44317d5b483b5b90ad567b980bc0a
JAUM519 <jorgeamedinau@gmail.com>
2025-11-20 11:21:18 -0500

    Cambios en las imagenes
    Agregadas y modificadas imagenes para la visualización de la pagina

--- smart-restaurant/src/data/menuSeeder.js
+++ smart-restaurant/src/data/menuSeeder.js
@@ -7,8 +7,8 @@ export const MENUS = [
   { id: 'frio', name: 'Fríos', parentId: 'cafe', kind: 'category' },
   { id: 'caliente', name: 'Calientes', parentId: 'cafe', kind: 'category' },
   // items (leafs) con price e img
-  { id: 'latte', name: 'Latte', parentId: 'caliente', kind: 'item', price: 8.5, img: '/vite.svg' },
-  { id: 'capuccino', name: 'Capuccino', parentId: 'caliente', kind: 'item', price: 9.0, img: '/vite.svg' },
-  { id: 'frappe', name: 'Frappe', parentId: 'frio', kind: 'item', price: 10.0, img: '/vite.svg' },
-  { id: 'brownie', name: 'Brownie', parentId: 'postres', kind: 'item', price: 6.0, img: '/vite.svg' },
+  { id: 'latte', name: 'Latte', parentId: 'caliente', kind: 'item', price: 8.5, img: '/menu/latte.jpg' },
+  { id: 'capuccino', name: 'Capuccino', parentId: 'caliente', kind: 'item', price: 9.0, img: '/menu/capuccino.jpg' },
+  { id: 'frappe', name: 'Frappe', parentId: 'frio', kind: 'item', price: 10.0, img: '/menu/frappe.jpeg' },
+  { id: 'brownie', name: 'Brownie', parentId: 'postres', kind: 'item', price: 6.0, img: '/menu/brownie.jpg' },
 ]
